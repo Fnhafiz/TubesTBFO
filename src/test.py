be5f988b40bbf0ce1013cbc650dad507a1c15c6e
Fitrah-16520207 <fitnug30@gmail.com>
2021-11-22 21:47:52 +0700

    testing

--- src/test.py
+++ src/test.py
@@ -2,9 +2,9 @@ from var import function
 import var
 for x, x, x, x in range(var, var, var):
 if var > var > var:
-def function(var, var, var, var, var):
-
-
+var(var, var, var)
+def var(var, var, var, var, var):
+var = [var for var in range(var)]
 class var:
 
     # Test untuk cek syntax

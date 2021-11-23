7a21afa302f6da03d94e0e17b8a1adb9b18eedeb
AkmalArifin <13520037@std.stei.itb.ac.id>
2021-11-23 08:44:13 +0700

    Fix Bug

--- src/replace_operator.py
+++ src/replace_operator.py
@@ -7,8 +7,10 @@ def replace_operator (line):
 
 '''Test Case Replace Operator'''
 
+'''
 line  = "100>=10"
 line2 = "if (j>=10) : "
 line3 = "if (A**2) == (B**3) :"
 line_new = replace_operator(line3)
 print(line_new)
+'''

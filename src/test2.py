fc6728e292519e442295ad06a38a3b1889a3d58d
Fitrah-16520207 <fitnug30@gmail.com>
2021-11-23 22:03:57 +0700

    tambahan testcase

---
+++ src/test2.py
@@ -0,0 +1,19 @@
+while 2 * 4 :
+    D=4
+[] is True
+x = [0 for T in range   (3,5)]
+N = int(input())
+c1 = input()
+c2 = input()
+if N<=0 or c1==c2:
+    print("Masukan tidak valid")
+else :
+    for x in range (0,N,4):
+        for y in range(0,N):
+            if x and y>0 and x<N-1 and y<N-1:
+                print(c2,end="")
+                K,J = 5,4,6*9
+            else :
+                print(c1,end="")
+        print("")
+
